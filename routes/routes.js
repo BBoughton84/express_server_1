52fc8f26a19149abf587493d7137e979e3db9fc1
Bill Boughton <boughton.william@gmail.com>
2017-03-26 14:35:41 -0600

    post still not working

--- routes/routes.js
+++ routes/routes.js
@@ -11,11 +11,20 @@ router.get('/', (req, res) => {
     })
 })
 
+//not working
 router.post('/post', (req, res) => {
-  var name = req.body.name
-  var make = req.body.make
-  var size = req.body.size
-  knex('dirtbike').insert({make:make, name:name, size:size})
+  var name1 = req.body.name
+  var make1 = req.body.make
+  var size1 = req.body.size
+  knex('dirtbike').insert({make:make1, name:name1, size:size1}).returning(['id', 'make', 'name', 'size'])
+    .then(result => {
+      res.send(200)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  var id = req.params.id
+  knex('dirtbike').where('id', id).del()
     .then(result => {
       res.send(200)
     })
